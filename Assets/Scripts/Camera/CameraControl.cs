345eb6e036147e751d18dbb4dbaf71e5c2e494dc
Kawtar CHOUBARI <choubchouba.kawtar@gmail.com>
2021-05-06 02:34:54 +0000

    Game Managers

--- Assets/Scripts/Camera/CameraControl.cs
+++ Assets/Scripts/Camera/CameraControl.cs
@@ -5,7 +5,7 @@ public class CameraControl : MonoBehaviour
     public float m_DampTime = 0.2f;                 
     public float m_ScreenEdgeBuffer = 4f;           
     public float m_MinSize = 6.5f;                  
-    /*[HideInInspector]*/ public Transform[] m_Targets; 
+    [HideInInspector] public Transform[] m_Targets; 
 
 
     private Camera m_Camera;                        
